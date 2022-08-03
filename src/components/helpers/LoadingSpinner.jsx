1dd91a43add2a5c24b291d07e9e7c73d1a763da5
MarkoMarkovikjEndava <marko.markovikj@endava.com>
2022-08-03 10:08:17 +0200

    fixed loader placement

--- src/components/helpers/LoadingSpinner.jsx
+++ src/components/helpers/LoadingSpinner.jsx
@@ -3,11 +3,13 @@ import "../../styles/LoadingSpinner.css";
 
 const LoadingSpinner = () => {
   return (
-    <div className="spinner">
-      <p className="loading-text">Getting Players</p>
-      <div className="spinner-sector spinner-sector-accent"></div>
-      <div className="spinner-sector spinner-sector-dark"></div>
-      <div className="spinner-sector spinner-sector-light"></div>
+    <div className="spinner-container">
+      <div className="spinner">
+        <p className="loading-text">Getting Players</p>
+        <div className="spinner-sector spinner-sector-accent"></div>
+        <div className="spinner-sector spinner-sector-dark"></div>
+        <div className="spinner-sector spinner-sector-light"></div>
+      </div>
     </div>
   );
 };
